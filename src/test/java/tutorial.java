a3f7a4f34202e66fbc065da46b203bc01f9de9fc
skyNatali <nanalu1@mail.ru>
2025-11-08 00:20:59 +0500

    fixed the code Swag Labs

---
+++ src/test/java/tutorial.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class tutorial {
+    @Test
+    public void username() {
+        WebDriver browser = new EdgeDriver();
+        browser.get("https://www.saucedemo.com/");
+        browser.findElement(By.xpath("//input[@data-test='username']")).sendKeys("standard_user");
+        browser.findElement(By.xpath("//input[@data-test='password']")).sendKeys("standard_user");
+        browser.findElement(By.xpath("//input[@value='login']")).click();
+
+        //browser.quit();
+    }
+}
