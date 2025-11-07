688b255f0843a90e6f91a03f4ee2952236380bc2
skyNatali <nanalu1@mail.ru>
2025-11-08 00:02:22 +0500

    Pom file added

---
+++ src/test/java/FirstClass.java
@@ -0,0 +1,44 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+public class FirstClass {
+    import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+import static java.lang.Thread.*;
+import static org.testng.Assert.*;
+
+    public class FirstClass {
+
+        @Test
+        public void zipCode() {
+            WebDriver browser = new EdgeDriver();
+            browser.get("https://www.sharelane.com/cgi-bin/register.py");
+            browser.findElement(By.xpath("//input[@name='zip_code']")).sendKeys("12345");
+            browser.findElement(By.xpath("//input[@value='Continue']")).click();
+            boolean registerBtnPresent = browser.findElement(By.xpath("//input[@value='Register']")).isDisplayed();
+            assertTrue(registerBtnPresent);
+            browser.quit();
+        }
+
+        @Test
+        public void zipDigitCode() throws InterruptedException {
+            WebDriver browser = new EdgeDriver();
+            browser.get("https://www.sharelane.com/cgi-bin/register.py");
+            browser.findElement(By.xpath("//input[@name='zip_code']")).sendKeys("12");
+            browser.findElement(By.xpath("//input[@value='Continue']")).click();
+            Thread.sleep(1000);
+            String errorText = browser.findElement(By.cssSelector(".error_message")).getText();
+            assertEquals(errorText, "Oops, error on page. ZIP code should have 5 digits");
+            browser.quit();
+        }
+    }
+
+}
