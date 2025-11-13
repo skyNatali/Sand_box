4c57d03b8bb19e92953a993b7e302a01959b8c4e
skyNatali <nanalu1@mail.ru>
2025-11-13 13:33:38 +0500

    fixed the code Swag Labs

--- src/test/java/pages/ProductsPage.java
+++ src/test/java/pages/ProductsPage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class ProductsPage extends BasePage {
-    private static final String ADD_TO_CART = "//button[@data-test='add-to-cart-sauce-labs-backpack']";
+    private static final String ADD_TO_CART = "//*[text()='%s']/ancestor::div[@class='inventory_item']//button[text()='Add to cart']";
 
     private By title = By.xpath("//span[@data-test='title']");
     public ProductsPage(WebDriver driver) {
@@ -17,5 +17,10 @@ public class ProductsPage extends BasePage {
 
     public String getTitleText() {
         return driver.findElement(title).getText();
+    }
+
+    public void addToCart(final String goodsName) {
+        By addToCart = By.xpath(ADD_TO_CART.formatted( goodsName));
+        driver.findElement(addToCart).click();
     }
 }
